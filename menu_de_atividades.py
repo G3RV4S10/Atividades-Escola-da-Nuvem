3f6eb0c1457871705cd3d3f664ecc0d259530141
Gervasio <147663211+G3RV4S10@users.noreply.github.com>
2025-10-28 00:49:26 +0000

    Adicionando um Menu de Atividades

---
+++ menu_de_atividades.py
@@ -0,0 +1,19 @@
+while True:
+    print("Selecione a Atividade: ")
+    print("Atividade 1 (1)")
+    print("Atividade 2 (2)")
+    print("Sair        (0)")
+    
+    opcao = input("Selecione a Atividade: ")
+    if opcao == "1":
+        print("Opção 1")
+    elif opcao == "2":
+        print("Opção 2")
+    elif opcao == "3":
+        print("Opcão 3")
+    elif opcao == "0":
+        print("Sair do programa")
+        break
+    else:
+        print("Opção Invalida")
+
